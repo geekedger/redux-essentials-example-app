abb7c4cc6b668d685c4800341a6031ad54a846d2
geekedger <geekedger@gmail.com>
2025-04-15 11:01:29 +0300

    feat: add users list

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import { EditPostForm } from './features/posts/EditPostForm'
 import { useAppSelector } from './app/hooks'
 import { LoginPage } from './features/auth/LoginPage'
 import { selectCurrentUsername } from './features/auth/authSlice'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const username = useAppSelector(selectCurrentUsername)
@@ -35,6 +37,8 @@ function App() {
                   <Route path="/posts" element={<PostsMainPage />} />
                   <Route path="/posts/:postId" element={<SinglePostPage />} />
                   <Route path="/editPost/:postId" element={<EditPostForm />} />
+                  <Route path="/users" element={<UsersList />} />
+                  <Route path="/users/:userId" element={<UserPage />} />
                 </Routes>
               </ProtectedRoute>
             }
