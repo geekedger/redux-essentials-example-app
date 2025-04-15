cce1d69d6dd468e6533f8763b8db6ddb770d76db
geekedger <geekedger@gmail.com>
2025-04-15 03:38:49 +0300

    feat: add spinner

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -3,12 +3,39 @@ import { PostAuthor } from './PostAuthor'
 import React, { useEffect } from 'react'
 import { useAppSelector, useAppDispatch } from '@/app/hooks'
 import { TimeAgo } from '@/components/TimeAgo'
-import { fetchPosts, selectAllPosts, selectPostsStatus } from './postsSlice'
+import { fetchPosts, Post, selectAllPosts, selectPostsError, selectPostsStatus } from './postsSlice'
+import { Spinner } from '@/components/Spinner'
+import { ReactionButtons } from './ReactionButtons'
+
+
+
+
+interface PostExcerptProps {
+  post: Post
+}
+
+function PostExcerpt({ post }: PostExcerptProps) {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+      </h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <ReactionButtons post={post} />
+    </article>
+  )
+}
 export const PostsList = () => {
   // Select the `state.posts` value from the store into the component
   const dispatch = useAppDispatch()
   const posts = useAppSelector(selectAllPosts)
   const postStatus = useAppSelector(selectPostsStatus)
+  const postsError = useAppSelector(selectPostsError)
+
 
   useEffect(() => {
     if (postStatus === 'idle') {
@@ -16,21 +43,39 @@ export const PostsList = () => {
     }
   }, [postStatus, dispatch])
 
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-  const renderedPosts = orderedPosts.map(post => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>
-      <Link to={`/posts/${post.id}`}>{post.title}</Link>
-      </h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-      <PostAuthor userId={post.user} />
-    </article>
-  ))
+  
+  let content: React.ReactNode
+
+  if (postStatus === 'pending') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map(post => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{postsError}</div>
+  }
+
+  // const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  // const renderedPosts = orderedPosts.map(post => (
+  //   <article className="post-excerpt" key={post.id}>
+  //     <h3>
+  //     <Link to={`/posts/${post.id}`}>{post.title}</Link>
+  //     </h3>
+  //     <p className="post-content">{post.content.substring(0, 100)}</p>
+  //     <PostAuthor userId={post.user} />
+  //   </article>
+  // ))
   
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   )
 }
--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -61,10 +61,21 @@ const initialReactions: Reactions = {
 //   },
 // ]
 
-export const fetchPosts = createAppAsyncThunk('posts/fetchPosts', async () => {
-  const response = await client.get<Post[]>('/fakeApi/posts')
-  return response.data
-})
+export const fetchPosts = createAppAsyncThunk(
+  'posts/fetchPosts',
+  async () => {
+    const response = await client.get<Post[]>('/fakeApi/posts')
+    return response.data
+  },
+  {
+    condition(arg, thunkApi) {
+      const postsStatus = selectPostsStatus(thunkApi.getState())
+      if (postsStatus !== 'idle') {
+        return false
+      }
+    }
+  }
+)
 
 
 const initialState: PostsState = {
